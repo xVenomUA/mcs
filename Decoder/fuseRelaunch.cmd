0c0ebf18b177bcff0e91e789dd9cc4959864142f
Pohrebniak Andrian <pogra29@gmail.com>
2024-03-04 22:09:32 +0200

    Push Decoder MKC LAB 1

---
+++ Decoder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/User/Desktop/1lab MKC/Decoder/My2To4Decoder_isim_beh.exe" -prj "C:/Users/User/Desktop/1lab MKC/Decoder/My2To4Decoder_beh.prj" "work.My2To4Decoder" "work.glbl" 
